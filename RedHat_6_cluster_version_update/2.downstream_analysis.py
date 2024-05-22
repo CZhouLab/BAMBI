4298008bd9508a234631e2682f74daa4ae239462
CZhouLab <88337187+CZhouLab@users.noreply.github.com>
2024-05-21 20:53:59 -0400

    Update 2.downstream_analysis.py

--- RedHat_6_cluster_version_update/2.downstream_analysis.py
+++ RedHat_6_cluster_version_update/2.downstream_analysis.py
@@ -20,9 +20,14 @@ import warnings
 def downstream_analysis(biomarker_target_gene_type="protein_coding", target_pvalue_type = "padj",
                          target_pvalue_threshold = 0.05, target_foldchange_threshold = 1.0,
                          target_maxmin_remove_threshold = 1.0, target_overlap_area_threshold = 0.01,
-                        dataset_name="Customized_Dataset", resubmit=False):
-    directory = os.getcwd()
-    output_directory = directory + "/output"
+                        dataset_name="Customized_Dataset", resubmit=False, HPC_parallel = False):
+    if not HPC_parallel:
+        directory = "/usr/src/app"
+        output_directory = os.getcwd() + "/output"
+    else:
+        directory = os.getcwd()
+        output_directory = directory + "/output"
+
     src_path = directory + "/src"
 
     statitical_based_feature_selection_gene_filter_script_path = src_path + "/downstream_analysis_scripts/2.statitical_based_feature_selection_gene_filter.py"
@@ -30,7 +35,7 @@ def downstream_analysis(biomarker_target_gene_type="protein_coding", target_pval
     result_collection_scirpt_path = src_path + "/downstream_analysis_scripts/4_result_collection.py"
 
     run_Pipeline_tem = subprocess.Popen(['python', statitical_based_feature_selection_gene_filter_script_path,
-                                         '--directory', directory,
+                                         '--directory', os.getcwd(), #directory,
                                          '--biomarker_target_gene_type', biomarker_target_gene_type,
                                          '--target_pvalue_type', target_pvalue_type,
                                          '--target_pvalue_threshold', str(target_pvalue_threshold),
@@ -41,14 +46,14 @@ def downstream_analysis(biomarker_target_gene_type="protein_coding", target_pval
     run_Pipeline_tem.communicate()
 
     run_Pipeline_tem = subprocess.Popen(['python', machine_learning_based_feature_selection_10CV_scirpt_path,
-                                         '--directory', directory,
+                                         '--directory', os.getcwd(), #directory,
                                          '--biomarker_target_gene_type', biomarker_target_gene_type,
                                          '--dataset_name', dataset_name])
 
     run_Pipeline_tem.communicate()
 
     run_Pipeline_tem = subprocess.Popen(['python', result_collection_scirpt_path,
-                                         '--directory', directory,
+                                         '--directory', os.getcwd(), #directory,
                                          '--biomarker_target_gene_type', biomarker_target_gene_type,
                                          '--dataset_name', dataset_name])
 
@@ -66,6 +71,7 @@ def main():
     parser.add_argument('-o', "--target_overlap_area_threshold", default=None, type=float, required=True)
     parser.add_argument('-n', "--dataset_name", default="Customized_Dataset", type=str, required=False)
     parser.add_argument("--r", type=str, default="False", help="whether resubmit termiated job" )
+    parser.add_argument('-h', "--HPC", type=str, default="FALSE", required=False)
     args = parser.parse_args()
 
     def t_or_f(fs):
@@ -75,10 +81,10 @@ def main():
         elif 'FALSE'.startswith(ua):
             return False
         else:
-            return False
+            return True
 
-    resubmit_requirment = t_or_f(args.r)
-
+    # resubmit_requirment = t_or_f(args.r)
+    HPC_parallel_opt = t_or_f(args.HPC)
     # Jobstatus_Check(biomarker_target_gene_type=args.biomarker_target_gene_type,
     #                 target_pvalue_type="padj",
     #                 target_pvalue_threshold=0.05, target_foldchange_threshold=1.0,
@@ -91,7 +97,7 @@ def main():
                         target_foldchange_threshold=args.target_foldchange_threshold,
                         target_maxmin_remove_threshold=args.target_maxmin_remove_threshold,
                         target_overlap_area_threshold=args.target_overlap_area_threshold,
-                        dataset_name=args.biomarker_target_gene_type, resubmit=False)
+                        dataset_name=args.biomarker_target_gene_type, resubmit=False, HPC_parallel=HPC_parallel_opt)
 
     # pipeline_step_1(sample_name=args.sample_name, R1_input=args.R1_input, R2_input=args.R2_input, strandness=args.strandness, directory=args.directory)
 
